dc65823d68b3f6fe4d9a764fe7b43e272c69ac4d
finleylee2507 <46691077+finleylee2507@users.noreply.github.com>
2021-11-24 10:40:54 -0600

    Updates

---
+++ precache-manifest.d423e19d0b44d24c6b51be79bdd1a12c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce44206807e4ef65d6aa",
+    "url": "/09-todo-react-finleylee2507/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "ce44206807e4ef65d6aa",
+    "url": "/09-todo-react-finleylee2507/static/js/main.ce442068.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/09-todo-react-finleylee2507/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "8f5becff6dca41d4593f",
+    "url": "/09-todo-react-finleylee2507/static/js/runtime~main.8f5becff.js"
+  },
+  {
+    "revision": "10e64f4e264bc2f8b5c49bb4ea174e12",
+    "url": "/09-todo-react-finleylee2507/index.html"
+  }
+];
